6a4b76fecbd2323eaa4e5d7a996937194f5c9b75
Eduardo Pereira dos Santos <144707466+EduEnd@users.noreply.github.com>
2024-09-06 12:03:37 -0300

    adicionando campo de nome professor, disciplina e turma

--- avaliação/JS/script.js
+++ avaliação/JS/script.js
@@ -3,24 +3,31 @@ const Iname = document.querySelector("#form_name");
 const Idescription = document.querySelector("#form_description");
 const Idate_evaluation = document.querySelector("#form_date_evaluation");
 const Inote = document.querySelector("#form_note");
+const Isubject = document.querySelector("#form_subject");  
+const Iclass = document.querySelector("#form_class");     
+const Iteacher = document.querySelector("#form_teacher");  
 
 function criarAvaliacao() {
     const avaliacaoData = {
         name: Iname.value,
         description: Idescription.value,
-        date_evaluation: Idate_evaluation.value, 
-        note: Inote.value
+        date_evaluation: Idate_evaluation.value,
+        note: Inote.value,
+        disciplina: Isubject.value,  
+        turma: Iclass.value,         
+        professor: Iteacher.value    
     };
 
     console.log("Dados enviados para o backend:", avaliacaoData);
 
+    
     let token = document.querySelector('meta[name="_csrf"]').getAttribute('content');
 
     fetch("http://localhost:8081/avaliacao/save", {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
-            'Authorization': 'Basic ' + btoa('user:'), //lembre de colocar o token
+            'Authorization': 'Basic ' + btoa('user:'), //Lembre de adicionar o token
             'X-CSRF-TOKEN': token
         },
         method: "POST",
@@ -46,6 +53,9 @@ function limpar() {
     Idescription.value = "";
     Idate_evaluation.value = "";
     Inote.value = "";
+    Isubject.value = "";   
+    Iclass.value = "";     
+    Iteacher.value = "";   
 }
 
 avaliacaoForm.addEventListener('submit', function(event) {
